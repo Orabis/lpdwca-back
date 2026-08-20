4b09444223687afbd27f372c5089f2ea31077473
lmerkel <lmerkel@unistra.fr>
2026-08-20 14:38:45 +0200

    add email provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default ({env}) => ({
+    email: {
+        config: {
+            provider: 'strapi-provider-email-resend',
+            providerOptions: {
+                apiKey: env('RESEND_API_KEY'),
+            },
+            settings: {
+                defaultFrom: env("RESEND_DEFAULT_EMAIL"),
+                defaultReplyTo: env("RESEND_USER_EMAIL"),
+            },
+        },
+    },
+});
